4599a946f46d914b18d355cb454ce8042a04241f
AylanJ123 <aylanjosue@gmail.com>
2023-06-20 16:49:51 -0600

    Cordyceps done, know needs to infect

--- Lua/AylanJ123/Parasytes/Afflictions/Hooks.lua
+++ Lua/AylanJ123/Parasytes/Afflictions/Hooks.lua
@@ -2,22 +2,8 @@
 Hook.Add("character.created", "testParasyteApply", function(character)
     local cordyceps = AfflictionPrefab.Prefabs["cordyceps"]
     local affl = cordyceps.Instantiate(
-        1, character
+        character.IsPlayer and 0.1 or math.random(1, 15), character
     )
-    if not character.IsPlayer then return end
+    if not character.CharacterHealth then return end
     character.CharacterHealth.ApplyAffliction(nil, affl, false)
-    local s, m = pcall(function ()
-        for _, v in pairs(affl.Prefab.Effects) do
-            print(v.MinStrength .. " -> " .. v.MaxStrength)
-        end
-    end)
-    if s then print(s)
-    else print(m) end
-    local ss, mm = pcall(function ()
-        for _, v in pairs(affl.Prefab.Descriptions) do
-            print(v.Text)
-        end
-    end)
-    if ss then print(ss)
-    else print(mm) end
  end)
